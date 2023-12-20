74ab621fac92a1c2a91c22e60baec97b2c9127ca
ahmed rakan <ar.aldhafeeri11@gmail.com>
2023-12-20 21:49:41 +0300

    update swagger

--- server/config/routes.ts
+++ server/config/routes.ts
@@ -7,9 +7,16 @@ export const PROJECT_ROUTE_REFRESH : string = "/project/refresh";
 export const RESOURCE_ROUTE : string = "/resource";
 export const DATA_ROUTE : string = "/data";
 export const ENDPOINT_ROUTE : string = "/endpoint";
+
 export const MOCK_ROUTE : string = "/mock";
 export const MOCK_ROUTE_RESOURCE_PARAM : string = "/:resourceName";
 export const MOCK_ROUTE_PAGINATE : string = "/paginate";
 export const MOCK_ROUTE_FILTER : string = "/filter";
 export const MOCK_ROUTE_SEARCH : string = "/search";
 export const MOCK_ROUTE_VALIDATE : string = "/validate";
+
+const RESOURCE_NAME_PARAM : string = "/:resourceName";
+const FUNCTION_NAME_PARAM : string = "/:functionName";
+
+export const EDGE_ROUTE : string = "/edge" + RESOURCE_NAME_PARAM + FUNCTION_NAME_PARAM;
+
