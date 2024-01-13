939f4c84ba8fa0292c26bd87697d6986a4fec2f4
ahmed rakan <ar.aldhafeeri11@gmail.com>
2024-01-13 08:44:00 +0300

    added - swagger ui libraries, rendered simple ui

--- client/src/components/containers/Swagger/Swagger.tsx
+++ client/src/components/containers/Swagger/Swagger.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import SwaggerUI from "swagger-ui-react"
+import "swagger-ui-react/swagger-ui.css";
 
 const Swagger  : React.FC = () => {
   return (
-    <div>Swagger</div>
+    <SwaggerUI spec={{
+      "openapi": "3.0.0",
+      "info": {
+        "title": "Hello World API",
+        "version": "1.0.0",
+        "description": "A simple API to greet the world"
+      },
+      "paths": {
+        "/hello": {
+          "get": {
+            "summary": "Get a friendly greeting",
+            "responses": {
+              "200": {
+                "description": "Successful response",
+                "content": {
+                  "application/json": {
+                    "example": {
+                      "message": "Hello, World!"
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }} />
+
   )
 }
 
