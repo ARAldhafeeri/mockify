29d456d2b568e4592e3932e4c6292fb9d0abc738
Ahmed Rakan <ar.aldhafeeri11@gmail.com>
2024-03-20 23:15:43 +0300

    fix event page error handling

--- client/src/redux/features/event/eventThunk.ts
+++ client/src/redux/features/event/eventThunk.ts
@@ -4,38 +4,52 @@ import {
   IFetchedEventData
 } from "types/Event";
 import { ENDPOINTS } from "constants/endpoints";
-import { IAPINormalizedResponse } from "types/global";
 
 export const fetchEvents = createAsyncThunk(
   "event/fetch",
-  async (resourceName : string, thunkAPI) : Promise<IAPINormalizedResponse> => {
+  async (resourceName : string, thunkAPI) : Promise<any> => {
+    try {
       const res : any = await instance.get(ENDPOINTS.EVENT_PARAM(resourceName));
-      return  res.data?.data;
-
+      return   res.data?.data;
+    } catch (e : any) {
+      return thunkAPI.rejectWithValue(e.response.data)
+    }
   }
 );
 
 export const deleteEvent = createAsyncThunk(
   "event/delete",
-  async(id : string, thunkAPI): Promise<IAPINormalizedResponse> => {
-    const res : any = await instance.delete(ENDPOINTS.EVENT_DELETE(id));
-    return res.data;
+  async(id : string, thunkAPI): Promise<any> => {
+    try {
+      const res : any = await instance.delete(ENDPOINTS.EVENT_DELETE(id));
+      return res.data;
+    } catch (e : any){ 
+      return thunkAPI.rejectWithValue(e.response.data)
+    }
   }
 )
 
 export const updateEvent = createAsyncThunk(
   "event/update", 
-  async(data : IFetchedEventData, thunkAPI): Promise<IAPINormalizedResponse> => {
-    const res : any = await instance.put(ENDPOINTS.EVENT, data);
-    return res.data;
+  async(data : IFetchedEventData, thunkAPI): Promise<any> => {
+    try {
+      const res : any =  instance.put(ENDPOINTS.EVENT, data);
+      return res;      
+    } catch (e : any){
+      return thunkAPI.rejectWithValue(e.response.data)
+    }
   }
 )
 
 export const createEvent = createAsyncThunk(
   "event/create",
-  async(data : IFetchedEventData, _): Promise<IAPINormalizedResponse> =>{
-    const res : any = await instance.post(ENDPOINTS.EVENT, data);
-    return res.data;
+  async(data : IFetchedEventData, thunkAPI): Promise<any> =>{
+    try {
+      const res : any = await instance.post(ENDPOINTS.EVENT, data);
+      return res.data;
+    } catch (e: any) {
+      return thunkAPI.rejectWithValue(e.response.data);
+    }
   }
 )
 
