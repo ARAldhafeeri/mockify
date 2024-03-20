80378ddd785c03bdb9711c6c0543da925588fae7
Ahmed Rakan <ar.aldhafeeri11@gmail.com>
2024-03-20 22:55:06 +0300

    fix resource page error handling

--- client/src/redux/features/endpoint/endpointThunk.ts
+++ client/src/redux/features/endpoint/endpointThunk.ts
@@ -1,14 +1,16 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import instance from "../../instance";
 import { ENDPOINTS } from "constants/endpoints";
-import { IAPINormalizedResponse } from "types/global";
 import { IFetchResourceResponse, IFetchedResourceData } from "types/Resource";
 
 export const fetchEndpoints = createAsyncThunk(
   "endpoint/fetch",
-  async (resource : IFetchedResourceData , thunkAPI) : Promise<IAPINormalizedResponse> => {
-      const res : any = await instance.post(ENDPOINTS.ENDPOINT, resource);
-      return  res.data?.data;
-
+  async (resource : IFetchedResourceData , thunkAPI) : Promise<any> => {
+    try {
+      const res : any = await instance.get(ENDPOINTS.ENDPOINT);
+      return  res.data;
+    } catch (e : any) {
+      return thunkAPI.rejectWithValue(e.response.data)
+    }
   }
 );
