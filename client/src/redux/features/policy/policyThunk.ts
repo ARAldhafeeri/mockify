8611d14aa14430aa5f31788300b9cf966522e290
Ahmed Rakan <ar.aldhafeeri11@gmail.com>
2024-03-20 22:51:36 +0300

    fix - policy page error handling

--- client/src/redux/features/policy/policyThunk.ts
+++ client/src/redux/features/policy/policyThunk.ts
@@ -4,36 +4,51 @@ import {
   IFetchedPolicyData
 } from "types/Policy";
 import { ENDPOINTS } from "constants/endpoints";
-import { IAPINormalizedResponse } from "types/global";
 
 export const fetchPolicy = createAsyncThunk(
   "policy/fetch",
-  async (projectID: string , thunkAPI) : Promise<IAPINormalizedResponse> => {
+  async (projectID: string , thunkAPI) : Promise<any> => {
+    try {
       const res : any = await instance.get(ENDPOINTS.DATA_POLICY_NAME(projectID));
       return  res.data;
+    } catch (e : any) {
+      return thunkAPI.rejectWithValue(e.response.data)
+    }
   }
 );
 
 export const deletePolicy = createAsyncThunk(
   "policy/delete",
-  async(id : string, thunkAPI): Promise<IAPINormalizedResponse> => {
-    const res : any = await instance.delete(ENDPOINTS.POLICY_DELETE(id));
-    return res.data;
+  async(id : string, thunkAPI): Promise<any> => {
+    try {
+      const res : any = await instance.delete(ENDPOINTS.POLICY_DELETE(id));
+      return res.data;
+    } catch (e : any){
+      return thunkAPI.rejectWithValue(e.response.data)
+    }
   }
 )
 
 export const updatePolicy = createAsyncThunk(
   "policy/update", 
-  async(policy : IFetchedPolicyData, thunkAPI): Promise<IAPINormalizedResponse> => {
-    const res : any = await instance.put(ENDPOINTS.POLICY, policy);
-    return res.data;
+  async(policy : IFetchedPolicyData, thunkAPI): Promise<any> => {
+    try {
+      const res : any =  instance.put(ENDPOINTS.POLICY, policy);
+      return res;      
+    } catch (e : any){
+      return thunkAPI.rejectWithValue(e.response.data)
+    }
   }
 )
 
 export const createPolicy = createAsyncThunk(
   "policy/create",
-  async(policy : IFetchedPolicyData, _): Promise<IAPINormalizedResponse> =>{
-    const res : any = await instance.post(ENDPOINTS.POLICY, policy);
-    return res.data;
+  async(policy : IFetchedPolicyData, thunkAPI): Promise<any> =>{
+    try {
+      const res : any = await instance.post(ENDPOINTS.POLICY, policy);
+      return res.data;
+    } catch (e: any) {
+      return thunkAPI.rejectWithValue(e.response.data);
+    }
   }
 )
