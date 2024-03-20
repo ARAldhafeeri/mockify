3ce4500387a4cc5fff16a358a79a48a68eb65212
Ahmed Rakan <ar.aldhafeeri11@gmail.com>
2024-03-20 23:19:56 +0300

    fix error handling client page

--- client/src/redux/features/client/clientThunk.ts
+++ client/src/redux/features/client/clientThunk.ts
@@ -4,38 +4,51 @@ import {
   IFetchedClientData
 } from "types/Client";
 import { ENDPOINTS } from "constants/endpoints";
-import { IAPINormalizedResponse } from "types/global";
 
 export const fetchClients = createAsyncThunk(
   "client/fetch",
-  async (projectId : string | undefined, thunkAPI) : Promise<IAPINormalizedResponse> => {
-      
-      const res : any = await instance.get(ENDPOINTS.CLIENT_PARAM(projectId));
-      return  res.data?.data;
-
+  async (projectId : string | undefined, thunkAPI) : Promise<any> => {
+      try {
+        const res : any = await instance.get(ENDPOINTS.CLIENT_PARAM(projectId));
+        return   res.data?.data;
+      } catch (e : any) {
+        return thunkAPI.rejectWithValue(e.response.data)
+      }
   }
 );
 
 export const deleteClient = createAsyncThunk(
   "client/delete",
-  async(id : string, thunkAPI): Promise<IAPINormalizedResponse> => {
-    const res : any = await instance.delete(ENDPOINTS.CLIENT_DELETE(id));
-    return res.data;
+  async(id : string, thunkAPI): Promise<any> => {
+    try {
+      const res : any = await instance.delete(ENDPOINTS.CLIENT_DELETE(id));
+      return res.data;
+    } catch (e : any){ 
+      return thunkAPI.rejectWithValue(e.response.data)
+    }
   }
 )
 
 export const updateClient = createAsyncThunk(
   "client/update", 
-  async(data : IFetchedClientData, thunkAPI): Promise<IAPINormalizedResponse> => {
-    const res : any = await instance.put(ENDPOINTS.CLIENT, data);
-    return res.data;
+  async(data : IFetchedClientData, thunkAPI): Promise<any> => {
+    try {
+      const res : any =  instance.put(ENDPOINTS.CLIENT, data);
+      return res;      
+    } catch (e : any){
+      return thunkAPI.rejectWithValue(e.response.data)
+    }
   }
 )
 
 export const createClient = createAsyncThunk(
   "client/create",
-  async(data : IFetchedClientData, _): Promise<IAPINormalizedResponse> =>{
-    const res : any = await instance.post(ENDPOINTS.CLIENT, data);
-    return res.data;
+  async(data : IFetchedClientData, thunkAPI): Promise<any> =>{
+    try {
+      const res : any = await instance.post(ENDPOINTS.CLIENT, data);
+      return res.data;
+    } catch (e: any) {
+      return thunkAPI.rejectWithValue(e.response.data);
+    }
   }
 )
