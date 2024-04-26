4aa444ee74292c2b1647494a926e9e3ca1664c2a
ahmed rakan <ar.aldhafeeri11@gmail.com>
2024-04-26 21:55:11 +0300

    added tailwind input

--- client/src/components/commons/Input/Input.tsx
+++ client/src/components/commons/Input/Input.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import IInputProps from "./Input.types";
 import { addtionalClasses } from "../../../utils";
-import { Input } from "antd";
 
 const MockifyInput : React.FC<IInputProps> = (props: IInputProps) => {
     const { classes, placeholder, type, label, onChange, name, value} = props;
     return (
-        <div className="mockify-input">
-            <Input  
-                className={addtionalClasses({classes: classes})}
+        <div className=" relative m-5">
+            <label className=" bg-light-primary px-2 text-xs rounded-full absolute top-[-10px] left-0">{label}</label>
+            <input  
+                className="w-[200px] shadow rounded py-2 px-3 l focus:ring-1 focus:ring-light-primary focus:outline-none"
                 placeholder={placeholder}
                 type={type}
                 name={name}
                 value={value}
                 onChange={onChange}
                 />
-            <label className="input-label">{label}</label>
         </div>
     );
 }
